c3ec95c2111ea2787f6f969f1c504131ae7cdeba
leekeunho <lkh890111@gmail.com>
2020-12-28 17:28:27 +0900

    마크업 스타일링
    - input, button 요소 추가 및 스타일 작업

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react' ; 
+import styled, { createGlobalStyle } from 'styled-components' ;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const 
+  GlobalStyle = createGlobalStyle`
+    html, body, #root {
+      height: 100% ; 
+      background:#eee ; 
+      display:flex ; 
+      justify-content : center; 
+      align-items:center ; 
+    }
+  ` 
+, SpinBoxWrap = styled.div`
+    display:inline-flex ; 
+    border-radius : 10px ;
+    overflow: hidden ; 
+  `
+, InputBlock = styled.input.attrs({
+    readOnly : true ,
+  })`
+    height : 60px ;
+    border:0;
+    font-size:24px;
+    text-align:center;
+    outline:none;
+    width:100px; 
+  `
+, BtnBlock = styled.button`
+    width:60px ; height:60px ; 
+    color:#fff ; 
+    background:#3498db ; 
+    border:0;
+    cursor:pointer;
+    font-size:24px;
+  `
+; 
+
+const SpinBox = _ => {
+  return(<>
+    <GlobalStyle />
+    <SpinBoxWrap>
+      <BtnBlock />
+      <InputBlock value={20} />
+      <BtnBlock />
+    </SpinBoxWrap>
+  </>)
 }
 
-export default App;
+export default SpinBox;
