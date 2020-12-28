091fe13503f4bb0c6c6c40ed4d784c8da2dd94b2
leekeunho <lkh890111@gmail.com>
2020-12-28 18:09:17 +0900

    기능 작업
    - 클릭 시 숫자 증가 감소 기능 추가
    - 계속 증가/감소 컨트롤 필요

--- src/App.js
+++ src/App.js
@@ -36,13 +36,86 @@ const
   `
 ; 
 
+const Btn = ({ type, setNum }) => {
+  const num = type ? 1 : -1 ; 
+  const [ loopBln, setLoopBln ] = useState( false ) ;
+  let timer = null ; 
+
+  const btnHandler = e => {
+
+    switch( e.type ) {
+      case 'click' :
+        console.log( 'click' ) ;
+        setNum(prevNum => prevNum += num ) ;
+        break ; 
+      case 'mousedown' :
+        console.log( 'mousedown' ) ;
+        timer = setTimeout(_=>{
+          setLoopBln( true ) ;
+        }, 1000) ;
+        break ;
+      case 'mouseup' : 
+        console.log( 'mouseup' ) ;
+        setLoopBln( false ) ; 
+        break ; 
+    }
+
+  }
+
+  useEffect(_=>{
+    if( !loopBln ) return ;
+    let timerLoop = null ; 
+    let startTime = null ;
+    let elapsedTime = 0 ; 
+
+    const cancelHandler = _ => {
+      console.log( 'cancelHandler in' ) ; 
+      clearTimeout( timer ) ;
+      cancelAnimationFrame( timerLoop ) ;
+      setLoopBln( false ) ; 
+    }
+
+    const loop = timestamp => {
+      console.log( 'loop in' ) ;
+      if( startTime == null ) startTime = timestamp ;
+      elapsedTime = Math.round( timestamp - startTime ) ;
+
+      setNum( prevNum => prevNum += num ) ; 
+
+      timerLoop = requestAnimationFrame( loop ) ;
+    }
+
+    timerLoop = requestAnimationFrame( loop ) ;
+    window.addEventListener( 'click' , cancelHandler ) ; 
+
+    // unmount
+    return _ => {
+      console.log( 'unmount in' ) ;
+      window.removeEventListener( 'click' , cancelHandler ) ; 
+      cancelHandler() ; 
+    }
+
+  }, [loopBln])
+
+  return (
+    <BtnBlock
+      onMouseDown={ btnHandler }
+      onMouseUp={ btnHandler }
+      onClick={ btnHandler }
+    >
+      {type && '+' || '-'}
+    </BtnBlock>
+  )
+} ;
+
 const SpinBox = _ => {
+  const [ num, setNum ] = useState( 0 ) ; // 기본값은 0
   return(<>
     <GlobalStyle />
     <SpinBoxWrap>
-      <BtnBlock />
-      <InputBlock value={20} />
-      <BtnBlock />
+      <Btn type={false} setNum={setNum} />
+      <InputBlock value={ num } />
+      <Btn type={true} setNum={setNum} />
     </SpinBoxWrap>
   </>)
 }
